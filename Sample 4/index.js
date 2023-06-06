47692ad72388426d4ebd325aa03b5cb61ca825d5
Darksteid <133988990+Darksteid@users.noreply.github.com>
2023-06-06 15:31:42 -0400

    Add files via upload

---
+++ Sample 4/index.js
@@ -0,0 +1,62 @@
+//Express app
+import express from 'express';
+import formidable from 'formidable';
+
+import { fileURLToPath } from 'url';
+import { dirname, parse, sep } from 'path';
+//config
+const
+	__dirname = dirname(fileURLToPath(import.meta.url)) + sep,
+	cfg = {
+		port: process.env.PORT || 3000,
+		dir: {
+			root: __dirname,
+			uploads: __dirname + 'uploads' + sep
+		}
+	};
+
+//Express init
+const app = express();
+//do not identify express
+app.disable('x-powered-by');
+//use EJS templates
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//static assets
+app.use(express.static(cfg.dir.uploads));
+
+//render form
+//use .all to handle both GET and POST
+app.all('/', (req, res, next) => {
+	if(req.method === 'GET' || req.method === 'POST'){
+		//parse file data
+		const form = formidable({
+			uploadDir: cfg.dir.uploads,
+			keepExtensions: true
+		});
+		form.parse(req, (err, data, files) => {
+			if(err){
+				next(err);
+				return;
+			}
+			
+			if(files && files.image && files.image.size > 0){
+				data.filename = files.image.originalFilename;
+				data.filetype = files.image.mimetype;
+				data.filesize = Math.ceil(files.image.size/1024) + ' KB';
+				data.uploadto = files.image.filepath;
+				data.imageurl = '/' + parse(files.image.filepath).base;
+			}
+			res.render('form', {title: 'Parse HTTP POST file data', data});
+		});
+	}
+	else{
+		next();
+	}
+});
+
+//start server
+app.listen(cfg.port, () => {
+	console.log('Example app listening at http://localhost:' + cfg.port);
+});
